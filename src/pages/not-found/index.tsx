7861958987160c6ade198f97debf07fbc635b264
Evgeniy Geraskin <daem0nbfg@gmail.com>
2022-04-03 21:44:06 +0300

    feat(init): not found page

--- src/pages/not-found/index.tsx
+++ src/pages/not-found/index.tsx
@@ -4,7 +4,11 @@ import styles from './styles.module.scss';
 
 const route = createRoute();
 
-const Page: React.FC = () => <div className={styles.Not}>404. Not found</div>;
+const Page: React.FC = () => (
+  <div className={styles.Not}>
+    <p>404. Not Found</p>
+  </div>
+);
 
 export const NotFoundPage = {
   Page,
